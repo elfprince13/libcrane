7c7b9e2ea2a9be0eceb10c5bdc794b2e6ccd19c2
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2015-09-25 12:42:39 -0400

    reworked as a CMake project

--- src/crane.cpp
+++ src/crane.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <fcntl.h>
-#include <string.h>
+#include <cstring>
 #include <sys/stat.h>
 #ifdef XDG_UNIX
 #include <dirent.h>
